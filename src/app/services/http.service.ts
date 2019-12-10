48a8417c60769fa01ba515ec6d53ddbda9dfa165
Zeeshan <zeeshan@allmyson.com>
2019-12-10 18:56:47 +0500

    http service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -16,7 +16,7 @@ export class HttpService {
 
   postPredictions(prediction) {
     // tslint:disable-next-line: max-line-length
-    let body = `match_id=${prediction.match_id}&user_id=${prediction.user_id}&Match=${prediction.Match}&ou25=${prediction.value_ou25}&1x2=${prediction.value_1x2}&1x2ht=${prediction.value_1x2ht}&ou05=${prediction.value_ou05}&ou15=${prediction.value_ou15}&ou05ht=${prediction.value_ou05ht}&ou15ht=${prediction.value_ou15ht}&ggng=${prediction.value_ggng}&double_change=${prediction.doubleChange}&status_id=${prediction.status_id}`;
+    let body = `match_id=${prediction.match_id}&user_id=${prediction.user_id}&Match=${prediction.Match}&ou25=${prediction.value_ou25}&1x2=${prediction.value_1x2}&1x2ht=${prediction.value_1x2ht}&ou05=${prediction.value_ou05}&ou15=${prediction.value_ou15}&ou05ht=${prediction.value_ou05ht}&ou15ht=${prediction.value_ou15ht}&ggng=${prediction.value_ggng}&double_change=${prediction.double_change}&status_id=${prediction.status_id}`;
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
     });
@@ -36,6 +36,7 @@ export class HttpService {
 
 
 
+  // accounts
 
   getChampions() {
     return this.http.get(this.apiurl + 'get_champions');
@@ -51,7 +52,7 @@ export class HttpService {
 
   postChampions(data) {
     // tslint:disable-next-line: max-line-length
-    let body = `name=${data.name}&sign=${data.sign}&isActive=${data.isActive}`;
+    let body = `name=${data.name}&nation_id=${data.nation_id}&sign=${data.sign}&isActive=${data.isActive}`;
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
     });
